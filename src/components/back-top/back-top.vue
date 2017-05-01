c8e83d648551ecf3ce6dbc32f1796e38afe1a684
wangyi <wy941009@gmail.com>
2017-05-01 19:56:00 +0800

    init back-top

---
+++ src/components/back-top/back-top.vue
@@ -0,0 +1,15 @@
+<template>
+    <div :class="classes"></div>
+</template>
+
+<script>
+const prefixCls = 'ant-back-top'
+
+export default {
+    computed: {
+        classes () {
+            return prefixCls
+        }
+    }
+}
+</script>
