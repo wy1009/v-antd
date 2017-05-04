d6760711c1b7b2c329d9064444bdc08036598d68
wangyi <wy941009@gmail.com>
2017-05-04 23:10:21 +0800

    init dropdown

---
+++ src/components/dropdown/dropdown.vue
@@ -0,0 +1,23 @@
+<template>
+    <div :class="classes">
+        <slot></slot>
+        <ul :class="menuClasses">
+            <slot name="list"></slot>
+        </ul>
+    </div>
+</template>
+
+<script>
+const prefixCls = 'ant-dropdown'
+
+export default {
+    computed: {
+        classes () {
+            return prefixCls
+        },
+        menuClasses () {
+            return `${prefixCls}-menu`
+        }
+    }
+}
+</script>
