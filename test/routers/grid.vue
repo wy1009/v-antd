f1be9702e5a63c736227d4d7cb250afdc87de314
wangyi <wy941009@gmail.com>
2017-05-07 17:36:17 +0800

    clear both

--- test/routers/grid.vue
+++ test/routers/grid.vue
@@ -1,9 +1,14 @@
 <template>
     <div class="grid">
         <row>
+            <v-col :span="8">col-8</v-col>
+            <v-col :span="8">col-8</v-col>
+            <v-col :span="8">col-8</v-col>
+        </row>
+        <row>
             <v-col :span="6">col-6</v-col>
             <v-col :span="6">col-6</v-col>
-            <v-col :span="6">col-6</v-col>
+            <v-col :span="12">col-12</v-col>
         </row>
     </div>
 </template>
@@ -11,6 +16,7 @@
 <style lang="sass">
 .grid {
     .ant-row {
+        margin-top: 10px;
         div {
             padding: 16px 0;
             background: #00a0e9;
